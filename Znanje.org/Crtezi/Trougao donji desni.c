e19288cfbf83567d57c4c39495160768679b78e1
Amina ₍ᐢ. .ᐢ₎ <159940527+divaaminz@users.noreply.github.com>
2025-02-06 20:12:21 +0100

    oui

--- Znanje.org/Crtezi/Trougao donji desni.c
+++ Znanje.org/Crtezi/Trougao donji desni.c
@@ -8,4 +8,23 @@
 
 #include <stdio.h>
 
+int main(){
+    int a,i,j;
+
+    printf("Unesite neki cijeli broj: ");
+    scanf("%d",&a);
+
+    for(i=0; i<a;i++){
+        for(j=0;j<a;j++){
+            if(j>i){
+                printf(" ");
+            }
+            if(j<=i){
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
