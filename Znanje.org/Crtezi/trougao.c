1e3c3e4686c0ed6cb468c383753df85a79189af7
Amina ₍ᐢ. .ᐢ₎ <159940527+divaaminz@users.noreply.github.com>
2025-02-06 21:10:23 +0100

    oui

---
+++ Znanje.org/Crtezi/trougao.c
@@ -0,0 +1,30 @@
+/*8.5.. Napisati program za ispis pravouglog trougla kao na slici sa željenim brojem redova:
+
+Unesite broj redova: 4
+X...
+.X..
+..X.
+...X
+*/
+
+#include <stdio.h>
+
+int main(){
+    int a,i,j,b;
+
+    printf("Unesite broj redova: ");
+    scanf("%d",&a);
+
+    for(i=1;i<=a;i++){
+        for(j=1;j<=a;j++){
+            if(j==i){
+                printf("X");
+            }
+            else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
